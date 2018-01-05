75e22e03fed2a24e902041a6ee14c24a247f229a
Victor Perez <vaperezr@unal.edu.co>
2018-01-04 21:06:52 -0500

    Changes for reducers

---
+++ reducers/index.js
@@ -0,0 +1,18 @@
+import { RECEIVE_ENTRIES, ADD_ENTRY } from '../actions'
+
+const entries = (state = {}, action) => {
+  switch (action.type) {
+    case RECEIVE_ENTRIES:
+      return {
+        ...state,
+        ...action.entries
+      }
+    case ADD_ENTRY:
+      return {
+        ...state,
+        ...action.entry
+      }
+    default:
+      return state
+  }
+}
